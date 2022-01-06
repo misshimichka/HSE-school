2b4d4d011d8c181d88f1176d741556210493fc9a
taisia <chegodaeva.stacy@gmail.com>
2022-01-06 11:39:07 +0300

    ленивая динамика: палиндромы

---
+++ main.py
@@ -0,0 +1,21 @@
+mem = {}
+
+
+def isPal(s):
+    return s == s[::-1]
+
+
+def f(s, i):
+    if i == len(s):
+        return 0
+    if i in mem:  #кеширование
+        return mem[i]
+    m = len(s)
+    for j in range(i, len(s)):
+        if isPal(s[i:j+1]):
+            m = min(m, 1 + f(s, j+1))
+    mem[i] = m
+    return m
+
+
+print(f("abaaaxyzyxxyyx", 0))
