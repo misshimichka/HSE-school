9d723762735483538ac369a4d44c8590c5864d2c
taisia <chegodaeva.stacy@gmail.com>
2022-01-06 12:20:54 +0300

    ленивая динамика: палиндромы на c++

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-mem = {}
-
 # Ленивая динамика: палиндромы
 def isPal(s):
     return s == s[::-1]
@@ -16,6 +14,29 @@ def f(s, i):
             m = min(m, 1 + f(s, j+1))
     mem[i] = m
     return m
+
+# динамика
+mem = {}
+s = 'asdfregsjj'
+n = len(s)
+
+isPmem = {}
+def isP(i, j):
+    if i >= j: return True
+    if (i,j) in isPmem: return isPmem[(i, j)]
+    f = (s[i] == s[j]) and isP(i+1, j-1)
+    isPmem[(i, j)] = f
+
+
+mem[n] = 0
+for i in range(n)[::-1]:
+    m = n
+    for j in range(i, n):
+        if isP(i, j):
+            m = min(m, 1 + mem[j + 1])
+    mem[1] = m
+print(mem[0])
+
 
 
 print(f("abaaaxyzyxxyyx", 0))
