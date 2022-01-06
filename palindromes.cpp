d76c9d9dc25e5e2ec0eac47275d2783f55813455
taisia <chegodaeva.stacy@gmail.com>
2022-01-06 12:06:04 +0300

    ленивая динамика: палиндромы на c++

---
+++ palindromes.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// n^3 -> n^2
+int isPal(string &s, int i, int j) {
+    if (i >= j)
+        return 1;
+    auto f = (s[i] == s[j] && isPal(s, i+1, j-1));
+    return f;
+}
+
+// 2^n * n --> n * n
+int f(string &s, int i) {
+    int n = s.size();
+    if (i == n)
+        return 0;
+    int m = n;
+    for (int j = i; i < n; j++) {
+        if (isPal(s, i, j)) {
+            m = min(m, 1, +f(s, j + 1));
+        }
+    }
+    return m;
+}
+int main() {
+    string s = "abaaaxyzyxxyyx";
+    cout << f(s, 1);
+
+    return 0;
+}
