2ce15e0c37be277dc2187494c8f2493ccce08f12
taisia <chegodaeva.stacy@gmail.com>
2022-01-06 13:20:29 +0300

    динамика

---
+++ program2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int f(int n) {
+    if (n == 0)
+        return 0;
+    int m = n;
+    for (int i = 1; i * i <= n; i++) {
+        m = min(m, 1 + f(n - i * i));
+    }
+    return m;
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << f(n) << endl;
+
+    return 0;
+}
